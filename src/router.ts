a0eb6a2bbefca5ab0dbc5252e60e5483f84254d3
kelvinsun <kelvinsun@tencent.com>
2019-03-26 11:20:20 +0800

    feat: editor page

--- src/router.ts
+++ src/router.ts
@@ -21,5 +21,13 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/editor',
+      name: 'editor',
+      // route level code-splitting
+      // this generates a separate chunk (editor.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Editor.vue'),
+    },
   ],
 });
---
+++ src/views/Editor.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>123</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: {},
+})
+export default class Editor extends Vue {}
+</script>
